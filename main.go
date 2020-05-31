02438e82664942d8d89a0d008896840a291ba57f
Dean Xu <dean.xu.2357@gmail.com>
2020-05-31 23:26:56 +0800

    add gracefull shutdown flow

--- main.go
+++ main.go
@@ -3,14 +3,55 @@ package main
 import (
 	c "auth/configs"
 	"auth/routes"
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func init() {
 	c.InitConfig()
 }
 
+func doAfterShutdown() {
+	log.Print("doing events after shutdown")
+}
+
 func main() {
-	r := routes.InitRouter()
-	_ = r.Run(fmt.Sprintf(":%v", c.Configs.Server.Port))
+	router := routes.InitRouter()
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%v", c.Configs.Server.Port),
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+
+	doAfterShutdown()
+
+	log.Println("Server exiting")
 }
--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"auth/configs"
 	v1 "auth/routes/Api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -8,6 +9,13 @@ import (
 func InitRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/health", v1.Health)
+	r.GET("/test_db", ShowDB)
 
 	return r
+}
+
+func ShowDB(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": configs.Configs.Database.DBName,
+	})
 }
