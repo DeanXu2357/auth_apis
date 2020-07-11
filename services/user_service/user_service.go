750195fcec948f1f8f2fad5dcd15ba29d0c9b452
Dean Xu <dean.xu.2357@gmail.com>
2020-07-11 23:01:13 +0800

    fix test

--- services/user_service/user_service.go
+++ services/user_service/user_service.go
@@ -13,16 +13,16 @@ func New(app *app.Instance) *UserService {
 	return &UserService{app: app}
 }
 
-func (service *UserService)GetUserByUUID(uuid string) (err error) {
+func (s *UserService)GetUserByUUID(uuid string) (user ,err error) {
 	return
 }
 
-func (service *UserService)Create(data map[string]interface{}) (err error) {
+func (s *UserService)Create(data map[string]interface{}) (err error) {
 	user := &models.User{
 		Name: data["name"].(string),
 		Email: data["email"].(string),
 	}
-	user.SetConnection(service.app.Database)
+	user.SetConnection(s.app.Database)
 	if err = user.Create(); err != nil {
 		return
 	}
--- services/user_service/user_service_test.go
+++ services/user_service/user_service_test.go
@@ -2,23 +2,14 @@ package user_service
 
 import (
 	a "auth/app"
-	c "auth/configs"
-	"fmt"
-	"github.com/jinzhu/gorm"
 	"github.com/smartystreets/assertions"
-	"github.com/stretchr/testify/assert"
-	"log"
 	"testing"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-func TestNew(t *testing.T) {
-	app, err := initApplication()
-	if err != nil {
-		assert.Fail(t, err.Error())
-	}
+func TestInitUserService(t *testing.T) {
+	app := a.New()
 	actual := New(app)
-
 	assertions.ShouldHaveSameTypeAs(actual, "UserService")
 }
 
@@ -28,26 +19,3 @@ func TestUserService_Create(t *testing.T) {
 func TestUserService_GetUserByUUID(t *testing.T) {
 }
 
-func initApplication() (application *a.Instance, err error) {
-	c.InitConfig()
-	config := c.GetConfigs()
-
-
-	dbInfo := fmt.Sprintf(
-		"host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
-		config.Database.DBHost,
-		config.Database.DBPort,
-		config.Database.DBUser,
-		config.Database.DBName,
-		config.Database.DBPassword)
-	db, err := gorm.Open("postgres", dbInfo)
-	if err != nil {
-		log.Printf("Database Connection failed : %s", err)
-		return
-	}
-	defer db.Close()
-
-	application = a.New(config, db)
-
-	return
-}
