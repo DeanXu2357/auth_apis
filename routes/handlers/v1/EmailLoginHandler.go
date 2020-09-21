3478fa5708c8a773686a3db7ef281b34cebf39e0
Dean Xu <dean.xu.2357@gmail.com>
2020-09-22 00:46:00 +0800

    refactor first api and add test

--- routes/handlers/v1/EmailLoginHandler.go
+++ routes/handlers/v1/EmailLoginHandler.go
@@ -3,6 +3,7 @@ package handlers_v1
 import (
 	"auth/app"
 	m "auth/models"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -19,11 +20,13 @@ type registerByMailInput struct {
 	Password string `json:"password" binding:"required"`
 }
 
+const EmailAlreadyRegistered = "email_already_registered"
+
 func NewEmailController(app *app.Instance) *EmailLoginHandler {
 	return &EmailLoginHandler{Application: app}
 }
 
-func (controller *EmailLoginHandler)RegisterByMail(c *gin.Context) {
+func (h *EmailLoginHandler)RegisterByMail(c *gin.Context) {
 	var input registerByMailInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -32,41 +35,30 @@ func (controller *EmailLoginHandler)RegisterByMail(c *gin.Context) {
 		return
 	}
 
-	tx := controller.Application.Database.Begin()
+	name := input.Name
+	email := input.Email
+	password := input.Password
 
-	if err := tx.Create(&m.User{Name: input.Name, Email: input.Email}).Error; err != nil {
-		tx.Rollback()
-		log.Print(err)
+	user, err := h.Register(name, email, password);
+	switch err.Error() {
+	case EmailAlreadyRegistered:
 		c.JSON(http.StatusBadRequest, gin.H{"status": 40009, "message": "email is already registered"})
 		return
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), 8)
-	if err != nil {
-		log.Print(err)
+	default:
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	if err := tx.Create(&m.EmailLogin{Email: input.Email, Password: string(hashedPassword)}).Error; err != nil {
-		tx.Rollback()
-		log.Print(err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	tx.Commit()
-
-	c.JSON(http.StatusOK, gin.H{"message": "success"})
+	c.JSON(http.StatusOK, gin.H{"message": "success", "user_id": user.ID})
 	return
 }
 
 
-func (controller *EmailLoginHandler)ResendMail(c *gin.Context)  {
+func (h *EmailLoginHandler)ResendMail(c *gin.Context)  {
 	//
 }
 
-func (controller *EmailLoginHandler)ActivateEmailRegister(c *gin.Context) {
+func (h *EmailLoginHandler)ActivateEmailRegister(c *gin.Context) {
 	//
 }
 
@@ -74,3 +66,29 @@ func hashPassword(pwd string) string {
 	return pwd
 }
 
+func (h *EmailLoginHandler)Register(name string, email string, password string) (*m.User, error) {
+	// todo : find a transaction manager library
+	tx := h.Application.Database.Begin()
+
+	user := &m.User{Name: name, Email: email}
+
+	if err := tx.Create(user).Error; err != nil {
+		tx.Rollback()
+		return nil, errors.New(EmailAlreadyRegistered)
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 8)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := tx.Create(&m.EmailLogin{Email: email, Password: string(hashedPassword)}).Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	tx.Commit()
+
+
+	return user, nil
+}
---
+++ tests/unit/handlers/EmailLoginHandlerTest.go
@@ -0,0 +1,24 @@
+package handler_tests
+
+import (
+	handlers_v1 "auth/routes/handlers/v1"
+	"auth/tests"
+	"github.com/smartystreets/assertions"
+	"testing"
+)
+
+func Test_RegisterSuccess(t *testing.T) {
+	app := tests.InitialTestingApplication()
+	tests.RefreshDatabase(app)
+	h := handlers_v1.EmailLoginHandler{app}
+
+	name := "poyu"
+	email := "dean.dh@gmail.com"
+	password := "password"
+
+	user, err := h.Register(name, email, password)
+
+	assertions.ShouldNotBeNil(err)
+	assertions.ShouldHaveSameTypeAs(user, "User")
+	// todo: assert database has ...
+}
