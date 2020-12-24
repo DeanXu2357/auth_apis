aac966db20fe9b0c30465c7ee5258eb47d5e17c3
Dean <deanhsu@anuegroup.com.tw>
2020-12-24 10:46:27 +0800

    add email_verify model

---
+++ models/email_verify.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"github.com/satori/uuid"
+	"gorm.io/gorm"
+	"reflect"
+	"time"
+)
+
+type EmailVerify struct {
+	ID uuid.UUID `gorm:"type:uuid;primary_key;" fake:"{uuid}"`
+	Email string `gorm:"type:string;size:128;not null" fake:"email"`
+	MailType string `gorm:"type:string;size:64:not null" fake:"{randomstring:[verify,reset]}"`
+	verification int8 `gorm:"type:int;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func (u *EmailVerify)BeforeCreate(tx *gorm.DB) (err error) {
+	if u.ID != reflect.Zero(reflect.TypeOf(u.ID)).Interface() {
+		return
+	}
+
+	u.ID = uuid.NewV4()
+	return
+}
+
+func (u EmailVerify) TableName() string {
+	return "users"
+}
