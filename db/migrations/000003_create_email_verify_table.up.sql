e167d8f67c58b47d56c8ee368b37d7b4efeb0661
Dean <deanhsu@anuegroup.com.tw>
2020-12-24 10:47:03 +0800

    update schema

--- db/migrations/000003_create_email_verify_table.up.sql
+++ db/migrations/000003_create_email_verify_table.up.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS email_verify (
-    id SERIAL PRIMARY KEY,
+    id uuid NOT NULL,
     email VARCHAR (128),
+    mail_type VARCHAR (64),
     verification SMALLINT,
     created_at TIMESTAMP WITH TIME ZONE,
-    updated_at TIMESTAMP WITH TIME ZONE
+    updated_at TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (id)
 );
 
 COMMENT ON COLUMN email_verify.verification IS '0:未驗證, 1:已驗證';
+COMMENT ON COLUMN email_verify.mail_type IS 'verify:驗證信箱, reset:重置密碼';
