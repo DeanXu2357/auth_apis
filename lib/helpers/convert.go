c5004695967c124cfccfa3d3720b3f16841f1033
Dean <deanhsu@anuegroup.com.tw>
2020-12-28 16:59:13 +0800

    uuid output converter

--- lib/helpers/convert.go
+++ lib/helpers/convert.go
@@ -1,1 +1,28 @@
 package helpers
+
+import (
+	"encoding/base64"
+	"github.com/satori/uuid"
+	"strings"
+)
+
+var escaper = strings.NewReplacer("9", "99", "-", "90", "_", "91")
+var unescaper = strings.NewReplacer("99", "9", "90", "-", "91", "_")
+
+func UuidToShortString(u uuid.UUID) string {
+	return escaper.Replace(base64.RawURLEncoding.EncodeToString(u.Bytes()))
+}
+
+func ShortStringToUuid(s string) (uuid.UUID, error) {
+	dec, err := base64.RawURLEncoding.DecodeString(unescaper.Replace(s))
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	u, err := uuid.FromBytes(dec)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return u, nil
+}
---
+++ tests/unit/lib/convert_test.go
@@ -0,0 +1,29 @@
+package lib
+
+import (
+	"auth/lib/helpers"
+	"github.com/satori/uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_UuidToString(t *testing.T) {
+	uuidBytes := []byte{0x12, 0x3e, 0x45, 0x67, 0xe8, 0x9b, 0x12, 0xd3, 0xa4, 0x56, 0x42, 0x66, 0x55, 0x44, 0x00, 0x00}
+	shortString := "Ej5FZ90ibEtOkVkJmVUQAAA"
+	uuidObject, _ := uuid.FromBytes(uuidBytes)
+
+	s := helpers.UuidToShortString(uuidObject)
+
+	assert.Equal(t, s, shortString)
+}
+
+func Test_StringToUuid(t *testing.T) {
+	uuidBytes := []byte{0x12, 0x3e, 0x45, 0x67, 0xe8, 0x9b, 0x12, 0xd3, 0xa4, 0x56, 0x42, 0x66, 0x55, 0x44, 0x00, 0x00}
+	shortString := "Ej5FZ90ibEtOkVkJmVUQAAA"
+	uuidObject, _ := uuid.FromBytes(uuidBytes)
+
+	u, err := helpers.ShortStringToUuid(shortString)
+
+	assert.Nil(t, err)
+	assert.Equal(t, u, uuidObject)
+}
